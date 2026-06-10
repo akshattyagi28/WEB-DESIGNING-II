d0e37ebfc6cd8ed03a82a0d36c16ee138c5847d2
akshattyagi28 <akshattyagi2007may@gmail.com>
2026-06-10 21:25:23 +0530

    Update App.jsx

--- experiment 7/src/App.jsx
+++ experiment 7/src/App.jsx
@@ -7,13 +7,13 @@ function App() {
     <div className="main-box">
       <h1>Student Results</h1>
       <div className="container">
-        <Props name="Aayushi Sharma" course="Computer Science" marks="95" />
-        <Props name="Pihu Sharma" course="Computer Science" marks="92" />
-        <Props name="Girish" course="Computer Science" marks="98" />
+        <Props name="Akshat tyagi" course="Computer Science" marks="95" />
+        <Props name="Aryan tyagi" course="Computer Science" marks="92" />
+        <Props name="Aman tyagi" course="Computer Science" marks="98" />
       </div>
       <div className="footer-info">
-          <h3>Aayushi Sharma</h3>
-          <h4>Roll Number: 2503201000022</h4>
+          <h3>Akhat tyagis</h3>
+          <h4>Roll Number: 2503201000144</h4>
         </div>
     </div>
    </>
